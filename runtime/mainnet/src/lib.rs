454e5d77f2aa8f0921080d34023048f96ea6d33d
Peter White <23270067+peterwht@users.noreply.github.com>
2022-10-11 08:46:40 -0600

    Update runtime version to 1.1.0 and client to 0.9.27 (#6)
    * update runtime version to 1.1.0 and client to 0.9.270
    
    Co-authored-by: Ignacio Palacios <ignacio.palacios.santos@gmail.com>

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -181,7 +181,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("watr"),
 	impl_name: create_runtime_str!("watr"),
 	authoring_version: 1,
-	spec_version: 1000,
+	spec_version: 1100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
