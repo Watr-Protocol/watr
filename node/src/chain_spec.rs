6bf249f00b27e5f1de6e1a2d4b39278845c29fd3
Peter White <23270067+peterwht@users.noreply.github.com>
2022-10-11 04:51:31 -0600

    Updated chain-spec to separate mainnet and devnet. (#5)
    * Updated chain-spec to separate mainnet and devnet.
    - Renamed `runtime/watr` to `runtime/mainnet`
    - Added `runtime/common` to share configs and consts between runtimes
    - Updated chain-spec to separate mainnet and devnet
    - Updated command.rs to use the new chain-spec options
    - Deleted "node" from `spec_name` and `impl_name` in the runtimes
    
    * Small typo in CENTI_WATR -> CENTI_WATRD

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -6,10 +6,18 @@ use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public, H160, U256};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 use std::{collections::BTreeMap, str::FromStr};
-use watr_runtime::{AccountId, AuraId, BalancesConfig, Signature, EXISTENTIAL_DEPOSIT, WATR};
 
-/// Specialized `ChainSpec` for the normal parachain runtime.
-pub type ChainSpec = sc_service::GenericChainSpec<watr_runtime::GenesisConfig, Extensions>;
+use watr_common::{AccountId, AuraId, Signature};
+use watr_runtime as mainnet;
+use watr_runtime::WATR;
+
+use watr_devnet_runtime as devnet;
+use watr_devnet_runtime::WATRD;
+
+/// Specialized `MainnetChainSpec` for the normal parachain runtime.
+pub type MainnetChainSpec = sc_service::GenericChainSpec<mainnet::GenesisConfig, Extensions>;
+
+pub type DevnetChainSpec = sc_service::GenericChainSpec<devnet::GenesisConfig, Extensions>;
 
 /// The default XCM version to set in genesis config.
 const SAFE_XCM_VERSION: u32 = xcm::prelude::XCM_VERSION;
@@ -60,25 +68,29 @@ where
 /// Generate the session keys from individual elements.
 ///
 /// The input must be a tuple of individual keys (a single arg for now since we have just one key).
-pub fn template_session_keys(keys: AuraId) -> watr_runtime::SessionKeys {
-	watr_runtime::SessionKeys { aura: keys }
+pub fn mainnet_session_keys(keys: AuraId) -> mainnet::SessionKeys {
+	mainnet::SessionKeys { aura: keys }
 }
 
-pub fn development_config() -> ChainSpec {
+pub fn devnet_session_keys(keys: AuraId) -> devnet::SessionKeys {
+	devnet::SessionKeys { aura: keys }
+}
+
+pub fn devnet_development_config() -> DevnetChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("tokenSymbol".into(), "WATRD".into());
 	properties.insert("tokenDecimals".into(), 18.into());
 	properties.insert("ss58Format".into(), 19.into());
 
-	ChainSpec::from_genesis(
+	DevnetChainSpec::from_genesis(
 		// Name
 		"Development",
 		// ID
 		"dev",
 		ChainType::Development,
 		move || {
-			testnet_genesis(
+			devnet_testnet_genesis(
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// initial collators.
 				vec![
@@ -104,6 +116,67 @@ pub fn development_config() -> ChainSpec {
 					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					//prefunded EVM account
+					hex!["e1ad20aae239ccbb609aa537d515dc9d53c5936ea67d8acc9fe0618925279f7d"].into(),
+				],
+				PARA_ID.into(),
+				// Total supply
+				Some(12000000 * WATRD),
+			)
+		},
+		Vec::new(),
+		None,
+		None,
+		None,
+		None,
+		Extensions {
+			relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
+			para_id: PARA_ID,
+		},
+	)
+}
+
+pub fn mainnet_development_config() -> MainnetChainSpec {
+	// Give your base currency a unit name and decimal places
+	let mut properties = sc_chain_spec::Properties::new();
+	properties.insert("tokenSymbol".into(), "WATR".into());
+	properties.insert("tokenDecimals".into(), 18.into());
+	properties.insert("ss58Format".into(), 19.into());
+
+	MainnetChainSpec::from_genesis(
+		// Name
+		"Development",
+		// ID
+		"dev",
+		ChainType::Development,
+		move || {
+			mainnet_testnet_genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// initial collators.
+				vec![
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						get_collator_keys_from_seed("Alice"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						get_collator_keys_from_seed("Bob"),
+					),
+				],
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					//prefunded EVM account
 					hex!["e1ad20aae239ccbb609aa537d515dc9d53c5936ea67d8acc9fe0618925279f7d"].into(),
 				],
 				PARA_ID.into(),
@@ -123,21 +196,21 @@ pub fn development_config() -> ChainSpec {
 	)
 }
 
-pub fn local_testnet_config() -> ChainSpec {
+pub fn devnet_local_testnet_config() -> DevnetChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("tokenSymbol".into(), "WATRD".into());
 	properties.insert("tokenDecimals".into(), 18.into());
 	properties.insert("ss58Format".into(), 19.into());
 
-	ChainSpec::from_genesis(
+	DevnetChainSpec::from_genesis(
 		// Name
 		"Local Testnet",
 		// ID
 		"local_testnet",
 		ChainType::Local,
 		move || {
-			testnet_genesis(
+			devnet_testnet_genesis(
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// initial collators.
 				vec![
@@ -163,6 +236,73 @@ pub fn local_testnet_config() -> ChainSpec {
 					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					//prefunded EVM account
+					hex!["e1ad20aae239ccbb609aa537d515dc9d53c5936ea67d8acc9fe0618925279f7d"].into(),
+				],
+				PARA_ID.into(),
+				// Total supply
+				Some(12000000 * WATRD),
+			)
+		},
+		// Bootnodes
+		Vec::new(),
+		// Telemetry
+		None,
+		// Protocol ID
+		Some("watr-network"),
+		// Fork ID
+		None,
+		// Properties
+		Some(properties),
+		// Extensions
+		Extensions {
+			relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
+			para_id: PARA_ID,
+		},
+	)
+}
+
+pub fn mainnet_local_testnet_config() -> MainnetChainSpec {
+	// Give your base currency a unit name and decimal places
+	let mut properties = sc_chain_spec::Properties::new();
+	properties.insert("tokenSymbol".into(), "WATR".into());
+	properties.insert("tokenDecimals".into(), 18.into());
+	properties.insert("ss58Format".into(), 19.into());
+
+	MainnetChainSpec::from_genesis(
+		// Name
+		"Local Testnet",
+		// ID
+		"local_testnet",
+		ChainType::Local,
+		move || {
+			mainnet_testnet_genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// initial collators.
+				vec![
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						get_collator_keys_from_seed("Alice"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						get_collator_keys_from_seed("Bob"),
+					),
+				],
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					//prefunded EVM account
 					hex!["e1ad20aae239ccbb609aa537d515dc9d53c5936ea67d8acc9fe0618925279f7d"].into(),
 				],
 				PARA_ID.into(),
@@ -188,64 +328,186 @@ pub fn local_testnet_config() -> ChainSpec {
 	)
 }
 
-fn testnet_genesis(
+fn devnet_testnet_genesis(
 	root_key: AccountId,
 	invulnerables: Vec<(AccountId, AuraId)>,
 	endowed_accounts: Vec<AccountId>,
 	id: ParaId,
-	total_issuance: Option<watr_runtime::Balance>,
-) -> watr_runtime::GenesisConfig {
-	use watr_runtime::EVMConfig;
+	total_issuance: Option<devnet::Balance>,
+) -> devnet::GenesisConfig {
+	use devnet::EVMConfig;
 
 	let num_endowed_accounts = endowed_accounts.len();
 	let balances = match total_issuance {
 		Some(total_issuance) => {
 			let balance_per_endowed = total_issuance
-				.checked_div(num_endowed_accounts as watr_runtime::Balance)
-				.unwrap_or(0 as watr_runtime::Balance);
+				.checked_div(num_endowed_accounts as devnet::Balance)
+				.unwrap_or(0 as devnet::Balance);
 
 			endowed_accounts.iter().cloned().map(|k| (k, balance_per_endowed)).collect()
 		},
 		None => vec![],
 	};
 
-	watr_runtime::GenesisConfig {
-		system: watr_runtime::SystemConfig {
-			code: watr_runtime::WASM_BINARY
+	devnet::GenesisConfig {
+		system: devnet::SystemConfig {
+			code: devnet::WASM_BINARY
 				.expect("WASM binary was not build, please build it!")
 				.to_vec(),
 		},
 
-		balances: BalancesConfig { balances },
+		balances: devnet::BalancesConfig { balances },
 
-		parachain_info: watr_runtime::ParachainInfoConfig { parachain_id: id },
-		collator_selection: watr_runtime::CollatorSelectionConfig {
+		parachain_info: devnet::ParachainInfoConfig { parachain_id: id },
+		collator_selection: devnet::CollatorSelectionConfig {
 			invulnerables: invulnerables.iter().cloned().map(|(acc, _)| acc).collect(),
-			candidacy_bond: EXISTENTIAL_DEPOSIT * 16,
+			candidacy_bond: devnet::EXISTENTIAL_DEPOSIT * 16,
 			..Default::default()
 		},
-		session: watr_runtime::SessionConfig {
+		session: devnet::SessionConfig {
 			keys: invulnerables
 				.into_iter()
 				.map(|(acc, aura)| {
 					(
-						acc.clone(),                 // account id
-						acc,                         // validator id
-						template_session_keys(aura), // session keys
+						acc.clone(),               // account id
+						acc,                       // validator id
+						devnet_session_keys(aura), // session keys
 					)
 				})
 				.collect(),
 		},
-		sudo: watr_runtime::SudoConfig { key: Some(root_key) },
+		sudo: devnet::SudoConfig { key: Some(root_key) },
 		// no need to pass anything to aura, in fact it will panic if we do. Session will take care
 		// of this.
 		aura: Default::default(),
 		aura_ext: Default::default(),
 		parachain_system: Default::default(),
-		polkadot_xcm: watr_runtime::PolkadotXcmConfig { safe_xcm_version: Some(SAFE_XCM_VERSION) },
+		polkadot_xcm: devnet::PolkadotXcmConfig { safe_xcm_version: Some(SAFE_XCM_VERSION) },
 
 		// EVM compatibility
-		// evm_chain_id: EVMChainIdConfig { chain_id },
+		evm: EVMConfig {
+			accounts: {
+				let mut map = BTreeMap::new();
+				map.insert(
+					// H160 address of Alice dev account
+					// Derived from SS58 (42 prefix) address
+					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex chars)
+					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
+				map.insert(
+					// H160 address of Metamask dev account
+					// Derived from SS58 (42 prefix) address
+					// SS58: 5HAc4UzLYQuyjHbpEPicC7bAjnofHqRWYStRKqA5WfreMKWk
+					// hex: 0xe1ad20aae239ccbb609aa537d515dc9d53c5936ea67d8acc9fe0618925279f7d
+					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex chars)
+					H160::from_str("0xe31B11A052aFC923259949352B2f573a21301Ba4")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
+				map.insert(
+					// H160 address of CI test runner account
+					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
+				map.insert(
+					// H160 address for benchmark usage
+					H160::from_str("1000000000000000000000000000000000000001")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						nonce: U256::from(1),
+						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+						storage: Default::default(),
+						code: vec![0x00],
+					},
+				);
+				map
+			},
+		},
+		ethereum: Default::default(),
+		base_fee: Default::default(),
+	}
+}
+
+fn mainnet_testnet_genesis(
+	root_key: AccountId,
+	invulnerables: Vec<(AccountId, AuraId)>,
+	endowed_accounts: Vec<AccountId>,
+	id: ParaId,
+	total_issuance: Option<mainnet::Balance>,
+) -> mainnet::GenesisConfig {
+	use mainnet::EVMConfig;
+
+	let num_endowed_accounts = endowed_accounts.len();
+	let balances = match total_issuance {
+		Some(total_issuance) => {
+			let balance_per_endowed = total_issuance
+				.checked_div(num_endowed_accounts as mainnet::Balance)
+				.unwrap_or(0 as mainnet::Balance);
+
+			endowed_accounts.iter().cloned().map(|k| (k, balance_per_endowed)).collect()
+		},
+		None => vec![],
+	};
+
+	mainnet::GenesisConfig {
+		system: mainnet::SystemConfig {
+			code: mainnet::WASM_BINARY
+				.expect("WASM binary was not build, please build it!")
+				.to_vec(),
+		},
+
+		balances: mainnet::BalancesConfig { balances },
+
+		parachain_info: mainnet::ParachainInfoConfig { parachain_id: id },
+		collator_selection: mainnet::CollatorSelectionConfig {
+			invulnerables: invulnerables.iter().cloned().map(|(acc, _)| acc).collect(),
+			candidacy_bond: mainnet::EXISTENTIAL_DEPOSIT * 16,
+			..Default::default()
+		},
+		session: mainnet::SessionConfig {
+			keys: invulnerables
+				.into_iter()
+				.map(|(acc, aura)| {
+					(
+						acc.clone(),                // account id
+						acc,                        // validator id
+						mainnet_session_keys(aura), // session keys
+					)
+				})
+				.collect(),
+		},
+		sudo: mainnet::SudoConfig { key: Some(root_key) },
+		// no need to pass anything to aura, in fact it will panic if we do. Session will take care
+		// of this.
+		aura: Default::default(),
+		aura_ext: Default::default(),
+		parachain_system: Default::default(),
+		polkadot_xcm: mainnet::PolkadotXcmConfig { safe_xcm_version: Some(SAFE_XCM_VERSION) },
+
+		// EVM compatibility
 		evm: EVMConfig {
 			accounts: {
 				let mut map = BTreeMap::new();
