ebfee09b3d389ec9fe8b1bfe988c1c7661b9384c
Niall Moore <niall@watr.org>
2022-12-12 13:38:40 +0000

    feat(blake2): adding blake2 precompile

--- runtime/devnet/src/precompiles.rs
+++ runtime/devnet/src/precompiles.rs
@@ -2,6 +2,7 @@ use pallet_evm::{Precompile, PrecompileHandle, PrecompileResult, PrecompileSet};
 use sp_core::H160;
 use sp_std::marker::PhantomData;
 
+use pallet_evm_precompile_blake2::Blake2F;
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
@@ -17,8 +18,20 @@ where
 	pub fn new() -> Self {
 		Self(Default::default())
 	}
-	pub fn used_addresses() -> [H160; 7] {
-		[hash(1), hash(2), hash(3), hash(4), hash(5), hash(1024), hash(1025)]
+	pub fn used_addresses() -> [H160; 11] {
+		[
+			hash(1),
+			hash(2),
+			hash(3),
+			hash(4),
+			hash(5),
+			hash(6),
+			hash(7),
+			hash(8),
+			hash(9),
+			hash(1024),
+			hash(1025),
+		]
 	}
 }
 impl<R> PrecompileSet for FrontierPrecompiles<R>
@@ -36,6 +49,7 @@ where
 			a if a == hash(6) => Some(Bn128Add::execute(handle)),
 			a if a == hash(7) => Some(Bn128Mul::execute(handle)),
 			a if a == hash(8) => Some(Bn128Pairing::execute(handle)),
+			a if a == hash(9) => Some(Blake2F::execute(handle)),
 			// Non-Frontier specific nor Ethereum precompiles :
 			a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
 			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
