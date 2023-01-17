bdf2ce6a468c22ecdf11c4fc92bdcd3047765769
NachoPal <ignacio.palacios.santos@gmail.com>
2023-01-17 14:17:48 +0100

    benchmark scripts added

---
+++ scripts/benchmarks-ci.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+steps=2
+repeat=2
+runtimeName=$1
+benchmarkOutput=./runtime/$runtimeName/src/weights
+benchmarkRuntimeName="$runtimeName-dev"
+
+if [[ $runtimeName == "devnet" ]] || [[ $runtimeName == "mainnet" ]]; then
+    pallets=(
+        frame_system
+        pallet_assets
+        pallet_balances
+        pallet_multisig
+        pallet_session
+        pallet_utility
+        pallet_timestamp
+        pallet_collator_selection
+        cumulus_pallet_xcmp_queue
+        pallet_collective
+        pallet_identity
+        pallet_scheduler
+        pallet_treasury
+        pallet_membership
+    )
+else
+	echo "'$runtimeName' pallet list not found in benchmarks-ci.sh"
+	exit 1
+fi
+
+for pallet in ${pallets[@]}
+do
+	output_file="${pallet//::/_}"
+
+    target/release/watr-node benchmark pallet \
+		--chain=$benchmarkRuntimeName \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--pallet=$pallet  \
+		--extrinsic='*' \
+		--steps=$steps  \
+		--repeat=$repeat \
+		--json \
+		--header=./file_header.txt \
+		--output="${benchmarkOutput}/${output_file}.rs"
+done
