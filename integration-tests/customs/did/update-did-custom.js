8f900bff96e0888f9452b1cb6ddc0e20851258a4
José Molina <jose.molina@parity.io>
2023-08-15 21:51:01 +0200

    feat: add integration test for updating a did

---
+++ integration-tests/customs/did/update-did-custom.js
@@ -0,0 +1,37 @@
+const ethers = require('ethers');
+
+const contractFile = require('./compile.js');
+const contractABI = contractFile.abi;
+
+const createDid = async (context, ...args) => {
+	const {networkName, rpcPort, chainId, senderPrivKey, didPrecompileAddress} = args[0][0];
+	const {did, controller, authentication, assertion, services} = args[0][1];
+
+	const providerRPC = {
+		development: {
+			name: networkName,
+			rpc: `http://127.0.0.1:${rpcPort}`,
+			chainId,
+		},
+	};
+
+	const provider = new ethers.providers.StaticJsonRpcProvider(providerRPC.development.rpc, {
+		chainId: providerRPC.development.chainId,
+		name: providerRPC.development.name,
+	});
+
+	// Create a new instance of the Wallet class
+	const wallet = new ethers.Wallet(senderPrivKey, provider);
+
+	// Create a new instance of the Contract class
+	const contract = new ethers.Contract(didPrecompileAddress, contractABI, wallet);
+
+	try {
+		const tx = await contract.updateDid(did, controller, authentication, assertion, services, {gasLimit: 250000});
+		await tx.wait();
+	} catch (e) {
+		console.error(`\n⚠️  WARNING: Eth tx failed\n`, e);
+	}
+}
+
+module.exports = createDid
