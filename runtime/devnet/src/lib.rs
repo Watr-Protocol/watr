21f750c531375c983e52391c8f5ac6efb93a902b
Peter White <23270067+peterwht@users.noreply.github.com>
2023-08-16 13:52:35 -0600

    chore: bump runtime version to 1240 (#139)

--- runtime/devnet/src/lib.rs
+++ runtime/devnet/src/lib.rs
@@ -310,7 +310,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("watr-devnet"),
 	impl_name: create_runtime_str!("watr-devnet"),
 	authoring_version: 1,
-	spec_version: 1230,
+	spec_version: 1240,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
